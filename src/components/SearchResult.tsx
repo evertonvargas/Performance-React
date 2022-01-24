20a545ad7c91efa9eea48e998641fa5fdbfdc5b5
everton vargas <evertonpassa7@gmail.com>
2022-01-24 17:10:35 -0300

    usememo - como usar

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,3 +1,9 @@
+/*
+useMemo é usado qnd temos algo que consome muito processamento e o resultado não é alterado a cada renderização
+Ex: cálculo
+*/
+
+import { useMemo } from 'react';
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultProps {
@@ -9,8 +15,17 @@ interface SearchResultProps {
 }
 
 export function SearchResults({results}: SearchResultProps){
+  // resultado não se altera e consome processamento toda vez
+  const totalPrice = useMemo(() => {
+    return results.reduce((acc, product) => {
+      return acc + product.price;
+  }, 0);
+}, [results]);
+
   return (
     <div>
+      <h2>Total: {totalPrice}</h2>
+
       {results.map((product, index) => {
         return(
           <ProductItem key={index} product={product} />
@@ -19,3 +34,11 @@ export function SearchResults({results}: SearchResultProps){
     </div>
   );
 }
+
+/*
+  Quando usar useMemo
+
+  1. Cálculos pesados
+  2. Quando passa aquela informação para um componente filho, a propriedade não muda por causa do useMemo e dessa forma não causa uma renderização desnecessária.
+  
+*/
