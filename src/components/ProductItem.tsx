a7cf081ef5ece57ffbf1e5f0502916ec08e8a345
everton vargas <evertonpassa7@gmail.com>
2022-01-24 16:23:32 -0300

    memo-como usar

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,16 @@
+/* 
+memo é uma função que usa por volta da exportação de um componente
+Evita que se crie uma nova versão do componente se nem uma props for mudada
+
+Fluxo do React
+1. Criar nova versão do componente
+2. Comparar com a versão anterior
+3. Se for diferente, criar nova versão
+
+*/
+
+import { memo } from 'react';
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +19,44 @@ interface ProductItemProps {
   }
 }
 
-export function ProductItem({product}: ProductItemProps) {
+//1 forma de usar o memo
+
+function ProductItemComponent({product}: ProductItemProps) {
   return (
     <div>
       <p>{product.name} - <strong>{product.price}</strong></p>
     </div>
   );
 }
+
+/*
+  memo faz shallow compare - comparação rasa
+  {} === {} -> false
+  [] === [] -> false
+  fizemos essa comparação para array e objeto, não ser rasa 
+  só que object.is dependendo do tamanho pode consumir processamento
+*/
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product);
+});
+
+
+//2 forma de usar o memo
+
+// export const ProductItem = memo(({product}: ProductItemProps) => {
+//   return (
+//     <div>
+//       <p>{product.name} - <strong>{product.price}</strong></p>
+//     </div>
+//   );
+// })
+
+/*
+  Quando usar memo
+
+  1. Componentes que renderizam frequentemente ou com as msm props
+  2. Componentes que não dependem de api ou algo pra atualizar, apenas as props
+  3. Componentes grandes
+  
+*/
